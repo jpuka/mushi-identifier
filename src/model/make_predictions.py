a765cbde49a21fb761ee625abc6ccfeb130d844d
jpusa <49599941+jpusa@users.noreply.github.com>
2021-09-25 15:33:37 +0300

    Refine: results to dict, paths, docs

--- src/model/make_predictions.py
+++ src/model/make_predictions.py
@@ -1,56 +1,64 @@
 # create predictions with the trained model
 
-## imports
+import pathlib
+
+## Imports
 import numpy as np
 import tensorflow as tf
 
-## load model
-model = tf.keras.models.load_model("models/mushi_model_1.keras")
+## Paths
+# Model directory
+path_model_dir = pathlib.Path(
+    "/home/jpe/Documents/python_projects/mushi-identifier/models/"
+)
 
-## load classes
+## Load model
+model = tf.keras.models.load_model(path_model_dir / "mushi_identifier_v1.keras")
 
-# could easily do this with pandas, but don't need to import anything this way
-path_classes = "/home/jpe/Documents/python_projects/mushi-identifier/models/classes_mushi_model_1.csv"
+## Load classes
+
+# Could easily do this with pandas, but don't need to import anything this way
+path_classes = path_model_dir / "classes_mushi_identifier_v1.csv"
 with open(path_classes, "r") as f:
     classes = [row.rstrip("\n") for row in f]
 
-# drop header
+# Drop header
 classes = classes[1:]
 
-## get image
+## Load image
+# TODO: make this cleaner, now just for testing and show
+
+# Path to image
 img_path = "/home/jpe/Documents/python_projects/mushi-identifier/data/00_external/2018_FGVCx_Fungi/interim_done/cantharellus_cibarius/APE2017-9196936_BkzOiYFVZ.JPG"
 
+# Load, resize image
 img = tf.keras.preprocessing.image.load_img(
     img_path, target_size=(224, 224, 3)
 )
 
-img.show()
-
-## preprocess image (mobilenet preprocessing attached to model though,
+## Preprocess image (mobilenet preprocessing attached to model though,
 # so just PIL -> array -> batches here)
 img_array = tf.keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)
 
-## make prediction
+## Create prediction
 prediction = model.predict(img_array)
 
-## apply softmax, since not used in model
+## Apply softmax to calculate confidences, since not done in model output layer
 prediction_softmax = tf.nn.softmax(prediction)
 
-## Calculate result
+## Find prediction indices and confidences
+pred_indices = np.flip(np.argsort(prediction_softmax).flatten())
+pred_confidences = prediction_softmax.numpy().flatten()
 
-prediction_args_sorted = np.flip(np.argsort(prediction_softmax).flatten())
-prediction_softmax_flat = prediction_softmax.numpy().flatten()
+## Save top 3 results to dictionary
+pred_classes_top3 = [classes[ix] for ix in pred_indices][:3]
+pred_confidences_top3 = [pred_confidences[ix] for ix in pred_indices][:3]
 
-prediction_classes_top_k = [None] * 3
-prediction_confidences_top_k = np.empty(3)
+predictions_top3 = dict(zip(pred_classes_top3, pred_confidences_top3))
 
-## Report result
-print("Top 3 predictions:")
-for k in range(3):
-    prediction_classes_top_k[k] = classes[prediction_args_sorted[k]]
-    prediction_confidences_top_k[k] = prediction_softmax_flat[prediction_args_sorted[k]]
-    print(
-        f"Prediction {k + 1}: {prediction_classes_top_k[k]} (Finnish name) "
-        f"Confidence: {prediction_confidences_top_k[k]:.3f}"
-    )
+## Print top 3 results
+print("-Top 3 predictions-", end="\n\n")
+# Dictionaries are ordered in python 3.7+, so this is safe
+for cls, conf in predictions_top3.items():
+    print(f"Prediction: {cls} \nConfidence: {conf:.3f}", end="\n\n")
