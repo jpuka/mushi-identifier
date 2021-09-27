8ea0889cefa7d85559ed64a6bd95949b878aeaad
jpusa <49599941+jpusa@users.noreply.github.com>
2021-09-27 23:27:07 +0300

    Refine baseline model script, create functions to file

---
+++ src/model/data_funcs.py
@@ -0,0 +1,186 @@
+# Functions for loading and plotting modeling data.
+
+## Imports
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from seaborn import heatmap
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+
+## Functions
+
+def load_dataset(path_train_val_dir, path_test_dir, train_val_split, seed=666,
+                 image_size=(224, 224), batch_size=32):
+    """
+    Load train, validation and test datasets from processed directory.
+
+    :param path_train_val_dir: Path to train/validation directory.
+    :param path_test_dir: Path to test directory.
+    :param train_val_split: Fraction of training data to reserve for validation.
+    :param seed: Random seed for train/validation split.
+    :param image_size: Size to resize images to after they are read from disk.
+    :param batch_size: Size of the batches of data.
+    :return: Train, validation and test datasets.
+    """
+    # TODO: Currently assumes that train and validation data are in the same directory
+    #  and split with this function. Once they have been pre-split to processed,
+    #  change this functionality to load them from separate directories.
+
+    # Load train dataset
+    train_dataset = image_dataset_from_directory(
+        path_train_val_dir,
+        validation_split=train_val_split,
+        subset="training",
+        seed=seed,
+        image_size=image_size,
+        batch_size=batch_size
+    )
+
+    # Load validation dataset
+    validation_dataset = image_dataset_from_directory(
+        path_train_val_dir,
+        validation_split=train_val_split,
+        subset="validation",
+        seed=seed,
+        image_size=image_size,
+        batch_size=batch_size
+    )
+
+    # Load test dataset
+    test_dataset = image_dataset_from_directory(
+        path_test_dir,
+        image_size=image_size,
+        batch_size=batch_size
+    )
+
+    return train_dataset, validation_dataset, test_dataset
+
+
+def plot_loss_accuracy(model_history=None, from_logs=False, path_training_logs=None):
+    """
+    Plot model loss and accuracy metrics from history object or from training logs.
+
+    :param model_history: Model history object. Required, if from_logs = False.
+    :param from_logs: Should the plots be drawn from history or training logs.
+    :param path_training_logs: Path to training logs. Required, if from_logs = True.
+    """
+
+    # If plots are to be drawn from logs
+    if from_logs:
+        # Check that path to log file has been defined
+        if not path_training_logs:
+            raise ValueError("Define path to the training log file")
+        # Read training logs
+        training_logs = pd.read_csv(path_training_logs)
+
+        # Read loss and metrics
+        acc = training_logs["acc"]
+        val_acc = training_logs["val_acc"]
+
+        top3_acc = training_logs["top_3_acc"]
+        val_top3_acc = training_logs["val_top_3_acc"]
+
+        loss = training_logs["loss"]
+        val_loss = training_logs["val_loss"]
+
+    # Otherwise read from the model history object
+    else:
+        # Check that the history object has been defined
+        if not model_history:
+            raise ValueError("Define the model history object")
+
+        # Read loss and metrics
+        acc = model_history.history["acc"]
+        val_acc = model_history.history["val_acc"]
+
+        top3_acc = model_history.history["top_3_acc"]
+        val_top3_acc = model_history.history["val_top_3_acc"]
+
+        loss = model_history.history["loss"]
+        val_loss = model_history.history["val_loss"]
+
+    # Initialize figure
+    plt.figure(figsize=(8, 8))
+
+    # Draw accuracies in first subplot
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, "C0", label="Training accuracy")
+    plt.plot(top3_acc, "C0--", label="Training top-3 accuracy")
+    plt.plot(val_acc, "C1", label="Validation accuracy")
+    plt.plot(val_top3_acc, "C1--", label="Validation top-3 accuracy")
+    plt.legend(loc="lower right")
+    plt.ylabel("Accuracy")
+    plt.ylim([0, 1])
+
+    # Draw losses in second subplot
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label="Training loss")
+    plt.plot(val_loss, label="Validation loss")
+    plt.legend(loc="upper right")
+    plt.ylabel("Categorical cross-entropy")
+
+    plt.xlabel("epoch")
+
+    # Show the figure
+    plt.show()
+    # plt.savefig("loss_accuracy.png")
+
+
+def create_confusion_matrix(model, ds):
+    """
+    Compute a confusion matrix from a dataset object by getting predicted and true
+    labels from batches of data.
+
+    :param model: Trained model.
+    :param ds: Dataset to find predicted labels and true labels for.
+    :return: Confusion matrix computed for the dataset.
+    """
+
+    # Initialize predicted and true labels
+    predicted_labels = np.array([])
+    true_labels = np.array([])
+
+    # Find predicted and true labels by looping over batches of data
+    for data_batch, label_batch in ds:
+        prediction = np.argmax(tf.nn.softmax(model.predict(data_batch)), axis=1)
+        predicted_labels = np.append(predicted_labels, prediction)
+        true_labels = np.append(true_labels, label_batch)
+
+    # Compute the confusion matrix
+    confusion_matrix = tf.math.confusion_matrix(true_labels, predicted_labels).numpy()
+
+    return confusion_matrix
+
+
+def plot_confusion_matrix(confusion_matrix, classes, normalize=True):
+    """
+    Plot the confusion matrix computed with create_confusion_matrix().
+
+    :param confusion_matrix: Confusion matrix from create_confusion_matrix().
+    :param classes: Class names corresponding to confusion matrix labels.
+    :param normalize: Should the confusion matrix be normalized prior to plotting.
+    """
+
+    if normalize:
+        # Normalize data to compensate for unbalanced classes
+        confusion_matrix = confusion_matrix / confusion_matrix.sum(axis=1, keepdims=True)
+        confusion_matrix = np.nan_to_num(confusion_matrix)
+        # Format labels as float
+        fmt = ".2f"
+    else:
+        # Format labels as integer
+        fmt = "d"
+
+    # Plot the confusion matrix
+    plt.figure(figsize=(16, 14))
+    heatmap(confusion_matrix, xticklabels=classes, yticklabels=classes, annot=True,
+            cmap="Blues", fmt=fmt)
+    plt.xlabel("Prediction")
+    plt.ylabel("Label")
+
+    # Show the figure
+    plt.show()
+    # plt.savefig("confusion_matrix.png")
--- src/model/make_model_v1.py
+++ src/model/make_model_v1.py
@@ -1,30 +1,37 @@
-# Here we build and train the model. There's also a notebook with the same code.
+# Build and train the baseline model, visualize the results. There's also a notebook
+# with the same code.
 
 ## Colab initialization
 # from google.colab import drive
 # drive.mount('/content/drive')
 
 ## Libraries
-import pathlib
+
 import csv
+import pathlib
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-## Paths
+from src.model.data_funcs import load_dataset, create_confusion_matrix, \
+    plot_confusion_matrix, plot_loss_accuracy
+
+## Paths (relative to project root)
+
+# Model name
+model_name = "mushi_identifier_v11"
 
 # Processed data directory
 path_processed_dir = pathlib.Path(
     # "drive/MyDrive/colab_data/02_processed/"
-    "/home/jpe/Documents/python_projects/mushi-identifier/data/02_processed/"
+    "data/02_processed/"
 )
 
 # Model directory
 path_model_dir = pathlib.Path(
     # "drive/MyDrive/colab_models/"
-    "/home/jpe/Documents/python_projects/mushi-identifier/models/"
+    "models/"
 )
 
 # Training and validation data
@@ -33,170 +40,170 @@ path_train_val_dir = path_processed_dir / "train_and_validation"
 # Test data
 path_test_dir = path_processed_dir / "test"
 
+# Training logs directory
+path_training_logs = path_model_dir / "training_logs" / (
+        model_name + "_training_logs.csv")
+
+# Names of the classes that the model is trained on
+path_model_class_names = path_model_dir / (model_name + "_classes.csv")
+
+# Saved model
+path_saved_model = path_model_dir / (model_name + ".keras")
+
 ## Import data
-# TODO: To function
 # TODO: Implement k-fold cross validation
 
 # MobileNetV2 max is 224 in Keras Danish Fungi authors used (299, 299) - possible
 # performance hit. Also makes the file size of our processed set feel unnecessarily big.
 image_size = (224, 224)
 
-# Batch size (TODO: test 64)
+# Batch size
+# TODO: test 64
 batch_size = 32
 
-# This is roughly a 80-10-10 split for the data
-
-train_dataset = image_dataset_from_directory(
-    path_train_val_dir,
-    validation_split=0.11,
-    subset="training",
-    seed=666,
-    image_size=image_size,
-    batch_size=batch_size
-)
-
-validation_dataset = image_dataset_from_directory(
-    path_train_val_dir,
-    validation_split=0.11,
-    subset="validation",
-    seed=666,
-    image_size=image_size,
-    batch_size=batch_size
-)
-
-test_dataset = image_dataset_from_directory(
-    path_test_dir,
-    image_size=image_size,
-    batch_size=batch_size
-)
+# This is roughly a 80-10-10 split for the data. The Danish Fungi article used 10 %
+# test data so we are matching validation dataset to that. But this is obviously too
+# little, and we should implement k-fold cross-validation to get reliable validation
+# scores.
+train_ds, validation_ds, test_ds = load_dataset(path_train_val_dir, path_test_dir,
+                                                train_val_split=0.11)
 
 ## Classes
 
 # Print class names
-classes = train_dataset.class_names
-print(classes)
-print(len(classes))
-
-# Set number of classes
-num_classes = len(train_dataset.class_names)
-
+class_names = train_ds.class_names
+print(class_names)
+print(len(class_names))
 
 # Save classes to file for use in predictions
-# TODO: save with Finnish name, do pandas tricks on classfile
-with open(path_model_dir / "classes_mushi_identifier_v1.csv", "w") as f:
+# TODO: Save with Finnish name, so they can be shown in predictions - use pandas to
+#  merge with class file
+with open(path_model_class_names, "w") as f:
     writer = csv.writer(f)
     writer.writerow(["species"])
-    writer.writerows(zip(classes))
+    writer.writerows(zip(class_names))
 
 ## View some images
 plt.figure(figsize=(10, 10))
-for images, labels in train_dataset.take(1):
+for images, labels in train_ds.take(1):
     for j in range(4):
         ax = plt.subplot(2, 2, j + 1)
         plt.imshow(images[j].numpy().astype("uint8"))
-        plt.title(train_dataset.class_names[labels[j]])
+        plt.title(train_ds.class_names[labels[j]])
         plt.axis("off")
 
+## Configure datasets for performance
 
-## Configure for performance (https://www.tensorflow.org/tutorials/load_data/images)
+# .cache() keeps the images in memory after they are loaded off disk during the first epoch.
+# .prefetch() overlaps data preprocessing and model execution while training.
 
 AUTOTUNE = tf.data.AUTOTUNE
-
-train_dataset = train_dataset.cache().prefetch(buffer_size=AUTOTUNE)
-validation_dataset = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
-test_dataset = test_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+validation_ds = validation_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 ## Augment images
+# TODO: add more augmentation (?)
 
-data_augmentation = tf.keras.Sequential([
+data_augmentation = keras.Sequential([
     # No vertical flip, mushroom pictures are usually not upside down
-    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
-    tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
-    tf.keras.layers.experimental.preprocessing.RandomZoom(0.2)
+    keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+    keras.layers.experimental.preprocessing.RandomRotation(0.1),
+    keras.layers.experimental.preprocessing.RandomZoom(0.2)
 ])
 
-## Look at augmented images
+## View some augmented images
 
 plt.figure(figsize=(10, 10))
-for images, _ in train_dataset.take(1):
+for images, _ in train_ds.take(1):
     for j in range(4):
         augmented_images = data_augmentation(images)
         ax = plt.subplot(2, 2, j + 1)
         plt.imshow(augmented_images[0].numpy().astype("uint8"))
         plt.axis("off")
 
-## Load base model
+## Load the base model used for feature extraction
 
 image_shape = image_size + (3,)
-base_model = tf.keras.applications.MobileNetV2(input_shape=image_shape,
-                                               include_top=False,
-                                               weights="imagenet")
+base_model = keras.applications.MobileNetV2(input_shape=image_shape,
+                                            include_top=False,
+                                            weights="imagenet")
 
-## Example batch output
-
-# image_batch, label_batch = next(iter(train_dataset))
-# feature_batch = base_model(image_batch)
-# print(feature_batch.shape)
-
-## Freeze conv base
+## Freeze the base model so its weights won't be updated during training
 
 base_model.trainable = False
 
 ## Build model
 
-inputs = tf.keras.Input(shape=image_shape)
+# Define number of classes = number of output neurons
+num_classes = len(class_names)
+
+inputs = keras.Input(shape=image_shape)
 x = data_augmentation(inputs)
 # Rescale images for MobileNetV2, which expects image scale [-1, 1]
-x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
-x = base_model(x, training=False)  # False due to the batch norm layers
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
-# https://github.com/keras-team/keras/issues/8470 - global better for small data!
-x = tf.keras.layers.Dropout(0.2)(x)  # TODO: tune
-outputs = tf.keras.layers.Dense(num_classes)(x)
+x = keras.applications.mobilenet_v2.preprocess_input(x)
+# Set training to False due to the batch normalization layers
+x = base_model(x, training=False)
+# Global pooling is better than flatten for small datasets. See:
+# https://github.com/keras-team/keras/issues/8470
+x = keras.layers.GlobalAveragePooling2D()(x)
+# TODO: Tune the dropout
+x = keras.layers.Dropout(0.2)(x)
+outputs = keras.layers.Dense(num_classes)(x)
 
-model = tf.keras.Model(inputs, outputs)
+model = keras.Model(inputs, outputs)
 
-# use activation "softmax" with from_logits=False
+# Use activation "softmax" with from_logits=False
 # or no activation and from_logits=True (latter might have more numerical stability:
 # https://datascience.stackexchange.com/questions/73093/what-does-from-logits-true-do
 # -in-sparsecategoricalcrossentropy-loss-function)
 
 ## Define metrics
-# TODO: Implement F1 macro? (not in keras metrics)
-# consider: precision, recall, roc curves, top-k accuracy (1, 3)
+
+# TODO: Implement macro F1 score (?), since it is not in keras metrics
+# TODO: Implement precision (and maybe recall), since they are only supported
+#  for binary classification https://github.com/tensorflow/addons/issues/1753
 
 metrics = [
-    keras.metrics.SparseTopKCategoricalAccuracy(k=1, name="top_1_acc"),
+    keras.metrics.SparseCategoricalAccuracy(name="acc"),  # "top 1 accuracy"
     keras.metrics.SparseTopKCategoricalAccuracy(k=3, name="top_3_acc")
+    # keras.metrics.Precision(name="prec")
+    # keras.metrics.Recall(name="rec")
 ]
 
 ## Define callbacks
-# TODO:
-#  -Check keras.callbacks.LearningRateScheduler for dynamically reducing l_rate
+
+# TODO: Use keras.callbacks.LearningRateScheduler for dynamically reducing learning
+#  rate
 
 callbacks = [
+    # Stop training once validation loss ceases to improve
     keras.callbacks.EarlyStopping(
         monitor="val_loss",
         verbose=1,
         patience=10
     ),
+    # Save the best model during training
     keras.callbacks.ModelCheckpoint(
-        filepath=path_model_dir / "mushi_identifier_v1.keras",
+        filepath=path_saved_model,
         monitor="val_loss",
         verbose=1,
         save_best_only=True
+    ),
+    # Log training loss and metrics to a csv file
+    keras.callbacks.CSVLogger(
+        filename=path_training_logs,
+        append=True
     )
 ]
 
 ## Compile model
 
-# TODO: tune
-learning_rate = 0.0001
+# TODO: Tune learning rate
+learning_rate = 0.001
 
-model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
               metrics=metrics)
-
 
 ## Colab tensorboard
 
@@ -214,18 +221,24 @@ model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=Tru
 
 ## Train model
 
-model_history = model.fit(train_dataset,
+model_history = model.fit(train_ds,
                           epochs=100,
                           callbacks=callbacks,
-                          validation_data=validation_dataset)
-
-# TODO: save history
-
-
-## Plot results
-
-# check the tensorflow imbalanced tutorial for plotting functions
-# confusion matrix
+                          validation_data=validation_ds)
 
 # lower accuracy can be random since we don't do k-fold cross valid
 # lower accuracy can be bc we have less classes, we use a subset
+
+
+## Load model
+# model = keras.models.load_model(filepath=path_saved_model)
+
+## Plot accuracy and loss
+
+# This looks insane, but it is related to the batch norm and dropout layers.
+plot_loss_accuracy(model_history)
+
+## Plot confusion matrix
+
+cm = create_confusion_matrix(model, validation_ds)
+plot_confusion_matrix(cm, class_names, normalize=False)
